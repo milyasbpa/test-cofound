78cfd42693a5a5f25bbd643d2824bf386c3e2340
milyasbpa <basilyas.2015@gmail.com>
2025-02-18 16:49:30 +0700

    fix: simulate user

--- user-service/src/kafka.ts
+++ user-service/src/kafka.ts
@@ -1,4 +1,5 @@
 import { Kafka } from "kafkajs";
+import { simulateUser } from "./user";
 
 const kafka = new Kafka({
   clientId: "user-service",
@@ -13,5 +14,22 @@ export async function sendEvent(event: any) {
   await producer.send({
     topic: "video-events",
     messages: [{ value: JSON.stringify(event) }],
+  });
+}
+
+// Spawn User
+export const consumer = kafka.consumer({ groupId: "user-group" });
+
+export async function startConsumer() {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "user-spawn", fromBeginning: false });
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const { count } = JSON.parse(message.value!.toString());
+      for (let i = 0; i < count; i++) {
+        simulateUser();
+      }
+    },
   });
 }
